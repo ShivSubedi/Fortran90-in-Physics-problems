6487020492d088bd3ba1d1806275b5799ade1ab5
ShivSubedi <31057907+ShivSubedi@users.noreply.github.com>
2020-12-29 20:36:40 -0500

    Add files via upload

---
+++ Assignment3/Problem1/single/forward/sqrt_x/x_1/diff.f90
@@ -0,0 +1,97 @@
+      
+!code provided by Dr. Elster, modified by S.Subedi
+program diff
+
+!        calculate forward differentiation of given function
+
+      implicit none
+
+!      double precision :: fcos, h, hd2, hd4, x, xmin, xmax, xstep
+!      double precision :: result(3)
+
+      real :: fsq, h, h2, hd4, x, xmin, xmax, xstep,ii,ana,error,ploep
+      real :: result(3)
+      integer :: kwrite,ploh
+
+      data kwrite/6/
+
+      open(kwrite, File = 'diff.dat', Status = 'Unknown')
+      
+      x=1
+      ana=0.5
+     
+
+!        forward
+      write (kwrite,*) 'DERIVATIVE OF SQRT(X) VIA CENTRAL DIFFERENTIATION METHOD FOR SINGLE PRECISION'
+      write(kwrite,*) 'For x=1'
+      write (kwrite,*) 'Taken analytical value: 1/(2*sqrt(1)) = 0.5 (Wolfram alpha)'
+      write (kwrite,*)
+      write (kwrite,*) '************************************************************************'
+      write (kwrite,*) '          h          1/(2*sqrt(0.5))     error   -log10(h)   log10(error)'
+      write (kwrite,*) '*************************************************************************'
+      do ii = 10,1,-1
+      h = 1E-10*10**ii
+      h2=h*0.5
+      
+      result(1) = (fsq(x+h2) - fsq(x))/h
+      error=abs((ana-result(1))/ana)*100
+!        !central
+      !result(2) = (fcos(x+hd2) - fcos(x-hd2))/h
+!        extrapolated
+      !result(3) = (8.d0 *(fcos(x+hd4)-fcos(x-hd4)) - &
+     !&                 (fcos(x+hd2)-fcos(x-hd2)))/(3.d0*h)
+     ploh=-log10(h2)
+     ploep=log10(error)
+
+      write (kwrite,10000) h2, result(1),error,ploh,ploep
+      end do
+
+10000 format(F16.9,2x,f16.9,4x,f7.2,5x,i3,6x,f7.2) 
+
+      close(kwrite)
+
+      stop 'data saved in diffplot.out'
+      
+      end program diff
+!--------------------------------------------------------------------------
+
+!      double precision function fcos(x)
+     !function fcos(x)
+
+      !implicit none
+
+!      double precision ::  x, fcos
+     !real ::  x, fcos
+
+      !fcos = cos(x)
+
+      !return
+      !end
+!--------------------------------------------------------------------------
+
+!      double precision function fexp(x)
+     ! function fexp(x)
+
+      !implicit none
+
+!      double precision ::  x, fexp
+      !real ::  x, fexp
+
+      !fexp = exp(x)
+
+      !return
+      !end
+!--------------------------------------------------------------------------
+
+      !function fsq(x)
+      function fsq(x)
+
+      implicit none
+
+    !double precision ::  x, fsq
+      real ::  x, fsq
+
+      fsq = sqrt(x)
+
+      return
+      end
